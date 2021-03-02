d61199cc6394e6bd89e2b96b2d9c4d21196c9988
Kamela Williamson <kami63116@yahoo.com>
2021-03-02 07:40:18 -0700

    added to notifications model

--- notification/models.py
+++ notification/models.py
@@ -9,3 +9,5 @@ class Notification(models.Model):
     tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, )
     profile = models.ForeignKey(TwitterUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
+    notification_total = models.IntegerField(default=0)
+    tweet_seen = models.BooleanField(default=False)
