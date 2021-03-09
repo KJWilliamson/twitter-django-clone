b1de5c68fd1fd2db7f14c325b9b7a22280a07c88
Kamela Williamson <kami63116@yahoo.com>
2021-03-08 20:01:44 -0700

    got buttons working. april helped with my template. i had it wrong

--- twitteruser/views.py
+++ twitteruser/views.py
@@ -3,7 +3,7 @@
 # https://learndjango.com/tutorials/django-slug-tutorial
 # https://docs.djangoproject.com/en/3.1/ref/class-based-views/base/
 
-from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.shortcuts import render, HttpResponseRedirect, redirect
 from django.contrib.auth.decorators import login_required
 from twitteruser.models import TwitterUser
 from tweet.models import Tweet
@@ -20,15 +20,21 @@ def index(request):
     following_count = len(main_user.following.all())
     home_tweets = Tweet.objects.filter(tweeter__in=request.user.following.all()).order_by('-created_at')
     users_list = TwitterUser.objects.all()
-    return render(request, 'index.html', {'all_tweets': all_tweets, 'tweet_count': tweet_count, 'following_count': following_count, 'users_list': users_list, 'home_tweets': home_tweets, 'main_user_tweets': main_user_tweets})
+    return render(request, 'index.html', {'all_tweets': all_tweets, 'tweet_count': tweet_count,
+                                          'following_count': following_count, 'users_list': users_list,
+                                          'home_tweets': home_tweets, 'main_user_tweets': main_user_tweets})
 
 
 def profile_view(request):
     all_tweets = Tweet.objects.all().order_by('created_at').reverse()
     profile_user = TwitterUser.objects.get(username=request.user)
-    tweet_count = Tweet.objects.filter(tweeter=request.user).order_by('-created_at')
+    tweet_counter = Tweet.objects.filter(tweeter__username=request.user).count()
+    follow_count = len(profile_user.following.all())
     profile_tweets = Tweet.objects.filter(tweeter=profile_user).order_by('created_at').reverse()
-    return render(request, 'profile.html', {'all_tweets': all_tweets, 'profile_user': profile_user, 'tweet_count': tweet_count, 'profile_tweets': profile_tweets})
+    following_list = TwitterUser.objects.all()
+    return render(request, 'profile.html', {'all_tweets': all_tweets, 'profile_user': profile_user,
+                                            'tweet_counter': tweet_counter, 'profile_tweets': profile_tweets,
+                                            'following_list': following_list, 'follow_count': follow_count})
 
 
 def user_detail_view(request, username):
@@ -42,15 +48,25 @@ def user_detail_view(request, username):
     return render(request, 'user_detail.html', {'current_user': current_user, 'user_tweets': user_tweets, 'following': following})
 
 
-def follow_view(request, username):
-    current_user = request.user
-    following_user = TwitterUser.objects.filter(username=username).first()
-    current_user.followers.add(following_user)
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+def follow_view(request, user_id):
+    current_users = TwitterUser.objects.get(id=user_id)
+    following_user = TwitterUser.objects.get(id=request.user.id)
+    following_user.followed_users.add(current_users)
+    following_user.following += 1
+    following_user.save()
+    current_users.user_followers.add(following_user)
+    current_users.followers += 1
+    current_users.save()
+    return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
 
 
-def unfollow_view(request, username):
-    current_user = request.user
-    following_user = TwitterUser.objects.filter(username=username).first()
-    current_user.followers.remove(following_user)
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+def unfollow_view(request, user_id):
+    current_users = TwitterUser.objects.get(id=user_id)
+    following_user = TwitterUser.objects.get(id=request.user.id)
+    following_user.followed_users.remove(current_users)
+    following_user.following -= 1
+    following_user.save()
+    current_users.user_followers.remove(following_user)
+    current_users.followers -= 1
+    current_users.save()
+    return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
